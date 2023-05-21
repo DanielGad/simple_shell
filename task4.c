cc8f859e4cf5dc9cd5e3f7a3fb903190f0c95eff
richie008 <richardabia74@gmail.com>
2023-05-21 13:18:42 -0700

    simple shel

---
+++ task4.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ *_eputts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _eputts(char *str)
+{
+	int k = 0;
+
+	if (!str)
+		return;
+	while (str[k] != '\0')
+	{
+		_eputchar(str[k]);
+		k++;
+	}
+}
+
+/**
+ * _eputcharr - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputcharr(char c)
+{
+	static int k;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || k >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, k);
+		k = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[k++] = c;
+	return (1);
+}
+
+/**
+ * _putfdd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfdd(char c, int fd)
+{
+	static int k;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || k >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, k);
+		k = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[k++] = c;
+	return (1);
+}
+
+/**
+ *_putsfdd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfdd(char *str, int fd)
+{
+	int k = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		k += _putfd(*str++, fd);
+	}
+	return (k);
+}
---
+++ task6.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+ **_strncpyy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpyy(char *dest, char *src, int n)
+{
+	int a, j;
+	char *s = dest;
+
+	a = 0;
+	while (src[a] != '\0' && a < n - 1)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	if (a < n)
+	{
+		j = a;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncatt - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncatt(char *dest, char *src, int n)
+{
+	int a, j;
+	char *s = dest;
+
+	a = 0;
+	j = 0;
+	while (dest[a] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[a] = src[j];
+		a++;
+		j++;
+	}
+	if (j < n)
+		dest[a] = '\0';
+	return (s);
+}
+
+/**
+ **_strchrr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchrr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+
